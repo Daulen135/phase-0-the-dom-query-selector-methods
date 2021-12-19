65164f30c6fe7d8e30f21bc7c3c065218a4a7f14
daulen <daulenz@gmail.com>
2021-12-19 08:53:28 -0600

    new lab done

---
+++ index.js
@@ -0,0 +1,12 @@
+let a = document.querySelector('div');
+console.log(a);
+a.innerHTML = "Webdevelopment";
+
+
+const li2 = document.querySelector('ul.ranked-list li ul li');
+console.log(li2);
+
+const div4 = document.querySelector('ul.unranked-list li div');
+console.log(div4);
+
+console.log(document.getElementById('app').querySelectorAll('ul.ranked-list li'));
